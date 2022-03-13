e44044daee988aa0689e561d8ac131cdb909b773
Mayil Kerimov <mayilik@code.edu.az>
2022-03-13 07:05:54 +0400

    Fourth commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.sidebar-menu i').click(function(){
+        $(this).parents('.dropdown').find('.dropdown').not($(this).siblings('ul')).slideUp();
+        $(this).siblings('ul').stop(true,true).slideToggle();
+        $(this).toggleClass('active');
+    });
+
+    $('#btn-open-navbar, #btn-close-navbar, .overlay').click(function(){
+        $('.overlay, .sidebar').toggleClass('active');
+    })
+});
